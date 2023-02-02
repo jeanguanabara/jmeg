d432acaffb2121738f4a91c2b9ddc796ec881369
jeanguanabara <jeanguanabara@gmail.com>
2023-02-02 00:14:07 -0300

    Tetando ajustar o js para edita meu cadastro

--- src/public/js/editaMeuCadastro.js
+++ src/public/js/editaMeuCadastro.js
@@ -3,14 +3,24 @@ window.addEventListener('load', ()=> {
     
     
 
-    const cboEstado = document.getElementById('cboEstado')
+    const name = document.getElementById('name')
+    const cpf = document.getElementById('cpf')
+    const email = document.getElementById('email')
+    const cep = document.getElementById('cep')
+    const estado = document.getElementById('cboEstado')
+    const cidade = document.getElementById('cidade')
+    const logradouro = document.getElementById('logradouro')
+    const complemento = document.getElementById('complemento')
+    
 
-    console.log('teste', cboEstado.selectedIndex)
+    console.log(cep)
+    
 
 
 
-    cboEstado.addEventListener('change',()=>{
-        console.log('teste', cboEstado.selectedIndex)  
+    cep.addEventListener('input',()=>{
+       console.log('Change para ',cep.value)
+        
     })
 
    
