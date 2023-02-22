753ffe7c3e1e78f28e8625136a2652d96b59a352
jeanguanabara <jeanguanabara@gmail.com>
2023-02-22 17:56:49 -0300

    Validações de login

--- src/middlewares/VerificaLoginCliente.js
+++ src/middlewares/VerificaLoginCliente.js
@@ -8,17 +8,17 @@ const VerificaLoginClienteMiddlaware = (req,res,next) => {
     console.log(senha)
 
 
-    if (Cliente.findEmail(email)){
+    if (Cliente.findEmail(email)){ //aqui verifica se tem o email do cliente na base
         console.log('Email bateu')
-        if (Cliente.findSenha(email,senha)){
+        if (Cliente.findSenha(email,senha)){ //se achar o email, vai verificar se a senha informada bate com o email que entrou
             console.log('Senha bateu')
             return next()
         }else {
-            console.log('Senha não bateu')
+            console.log('Senha não bateu') //se a senha não bater, vai retornar erro que a senha ta incorreta
             return res.render('login', {error: "Senha Incorreta"})
         }
     }else {
-        console.log('Email não bateu')
+        console.log('Email não bateu') //se o email ta incorreto nem valida senha, só diz que o email ta errado.
         return res.render('login', {error: "Email Incorreta"})
     }
 
