1293735b5b7c9350a3c24bb4654aa389db867706
eckstein75 <mauroeckstein@hotmail.com>
2023-02-22 15:24:00 -0300

    fazendo verifica login admin

---
+++ src/middlewares/VerificaLoginAdmin.js
@@ -0,0 +1,24 @@
+const VerificaLoginAdmin = (req,res,next) => {
+    let dadosLogin = req.query
+    let baseAdmin = require('../database/db.json')
+
+
+    for (let i in baseAdmin){
+
+        console.log(baseAdmin[i].email)
+        console.log(baseAdmin[i].senha)
+        
+        if (dadosLogin.vLoginEmailCliente === baseAdmin[i].email){
+            console.log('email bateu')
+            console.log(baseAdmin[i].email)
+          if (dadosLogin.vLoginSenhaAdmin == baseAdmin[i].senha){
+            console.log('senha bateu')
+             return next()
+          }
+        } 
+    }
+
+    return res.render('login', {error: 'Dados incorretos'})
+}
+
+    module.exports = VerificaLoginAdmin;
