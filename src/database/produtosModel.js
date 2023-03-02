99f610a1125241120c7c14cbdbedd8d5f9e7daa7
eckstein75 <mauroeckstein@hotmail.com>
2023-03-02 00:33:42 -0300

    configurando sequelize

--- src/database/produtosModel.js
+++ src/database/produtosModel.js
@@ -3,31 +3,29 @@ const fs = require('fs');
 const path = require('path');
 const pathDb = path.resolve("src", "database", "db.json");
 
-const productsModel = {
+const produtosModel = {
     //Método que procura todos os produtos do banco de dados
     findAll: () => {
-        return database.products;
+        return database.produtos;
     },
 
     //Método que procura um produto
     findByPk: (id) => {
-        const product = database.products.find(product => product.id === id);
-        return product;
+        const produto = database.produtos.find(produto => produto.id === id);
+        return produto;
     },
 
     //Método que criar um produto no banco de dados
-    create: (product) => {
-        database.products.push(product);
+    create: (produto) => {
+        database.produtos.push(produto);
 
         const dbJson = JSON.stringify(database);
         fs.writeFileSync(pathDb, dbJson, "utf8");
     },
 
     //Método que atualizar um produto no banco de dados
-    update: (id, product) => {},
+    update: (id, produto) => {},
 
     //Metodo que deleta um produto no banco de dados
     delete: (id) => {}
 }
-
-module.exports = productsModel;
--- src/models/Produto.js
+++ src/models/Produto.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const Product = sequelize.define('Produto', {
+    const Produto = sequelize.define('Produto', {
       id: {
         type: DataTypes.String,
         primaryKey: true
