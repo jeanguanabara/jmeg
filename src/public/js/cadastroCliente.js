35c614ad86c7ed5e23ae0659c53237e1f409ee40
jeanguanabara <jeanguanabara@gmail.com>
2023-03-07 17:49:18 -0300

    validações de cadastro do cliente

--- src/public/js/cadastroCliente.js
+++ src/public/js/cadastroCliente.js
@@ -2,7 +2,7 @@ window.addEventListener('load', ()=>{
 
     const formCadCliente    = document.getElementById('formCadCliente');
     const name              = document.getElementById('name');
-    const cpf               = document.getElementById('cpf');
+    const cpf               = document.getElementById('CPF');
     const email             = document.getElementById('email');
     const cep               = document.getElementById('cep');
     const uf                = document.getElementById('uf');
@@ -13,19 +13,81 @@ window.addEventListener('load', ()=>{
     const complemento       = document.getElementById('complemento');
     const senha1            = document.getElementById('senha1');
     const senha2            = document.getElementById('senha2');
+    
+    const campos = [name,cpf, email, cep, uf, cidade, bairro, logradouro,numero, complemento]
+    
+
+    //CONSULTA DE API COM CEP
+    cep.addEventListener('input',()=>{
+       
+       
+        if (cep.value.length > 7){
+ 
+         console.log(cep.value)
+         
+         fetch('https://viacep.com.br/ws/'+cep.value+'/json/')
+         .then(retornoViaCep => {
+             return retornoViaCep.json()
+         })
+         .then(retornoEndereco => {
+
+            console.log(retornoEndereco)
+             uf.value = retornoEndereco.uf.toUpperCase()
+             cidade.value = retornoEndereco.localidade.toUpperCase()
+             bairro.value = retornoEndereco.bairro.toUpperCase()
+             logradouro.value = retornoEndereco.logradouro.toUpperCase()
+         })
+        }
+         
+     })
+
+     
+
+     
+
+
+
+
+
+
+
+     //VALIDAÇÃO DO FORMULÁRIO
+    formCadCliente.addEventListener('submit', (event)=> {
+        event.preventDefault();
+
+
+        console.log('valida senha: '+ validaSenha(senha1,senha2))
+        console.log('valida campos: '+ validaCampos(campos))
+        
+    })
 
     
-    console.log("vai aparecer o campo name")
-    console.log(name)
-    console.log("apareceu name")
-    
-    name.addEventListener('change', () => {
-        console.log(name)
-    })
-    
-    formCadCliente.addEventListener('submit', (event)=> {
+
+    //VALIDAÇÃO DE CAMPOS PREENCHIDOS
+    function validaCampos (array) {
         
-        
-        event.preventDefault();
-    })
+
+        for (let i in array) {
+            if (array[i].value.length === 0){
+                return false
+            }
+        }
+
+        return true
+
+    }
+
+    //VALIDAÇÃO SE A SENHA É IGUAL
+    function validaSenha(a,b) {
+        if ((a.value.length > 0)&&(b.value.length > 0))  {
+            if (a.value === b.value) {
+                return true
+            }else {
+                return false
+            }
+        }else{
+            return false
+        }
+    }
+
 })
